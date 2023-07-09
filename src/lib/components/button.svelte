4fa7ac713f40226081f124aa2b5b3440995428fe
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-09 16:33:36 +0530

    updated btn

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -2,18 +2,21 @@
 	import type { HTMLButtonAttributes } from 'svelte/elements';
 	import { cva, type VariantProps } from 'class-variance-authority';
 
-	const buttonVariants = cva('text-white rounded-full transition-colors', {
-		variants: {
-			variant: {
-				primary: 'bg-primary hover:bg-primary-300',
-				secondary: 'bg-neutral-800'
-			},
-			size: {
-				small: 'h-[20px] px-1',
-				medium: 'h-[45px] px-4'
+	const buttonVariants = cva(
+		'text-white rounded-full flex items-center justify-center gap-1 transition-[background-color,box-shadow]',
+		{
+			variants: {
+				variant: {
+					primary: 'bg-primary hover:bg-primary-300 active:ring active:ring-primary',
+					secondary: 'bg-neutral-800'
+				},
+				size: {
+					small: 'h-[20px] px-1',
+					medium: 'h-[45px] px-4'
+				}
 			}
 		}
-	});
+	);
 
 	interface $$Props extends HTMLButtonAttributes, VariantProps<typeof buttonVariants> {}
 	export let variant: $$Props['variant'] = 'primary';
