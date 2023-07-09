a5b89593d7305c80b16d5a964bd6994a90606b76
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-09 18:05:01 +0530

    added connect wallet btn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,9 @@
 </script>
 
 <main class="h-screen flex flex-col items-center justify-center px-2 max-w-screen-sm mx-auto">
-	<form action="" method="post" class="flex flex-col gap-16">
-		<h1 class="font-bold text-2xl text-center">Book Your Ticket <Ticket /></h1>
-		<div class="flex flex-col gap-5">
+	<h1 class="font-bold text-2xl text-center mb-16">Book Your Ticket <Ticket /></h1>
+	<form action="" method="post" class="flex flex-col mb-16">
+		<div class="flex flex-col gap-5 mb-16">
 			<div>
 				<p class="mb-6">Origin:</p>
 				<Select values={stations} />
@@ -21,6 +21,7 @@
 				<Select values={stations} />
 			</div>
 		</div>
-		<Button on:click={() => alert(1)}>Make Payment <Cart /></Button>
+		<Button>Make Payment <Cart /></Button>
 	</form>
+	<button class="hover:opacity-75">Connect Wallet</button>
 </main>
