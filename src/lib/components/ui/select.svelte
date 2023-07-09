9ceb53b3d2949cab1b7383b4ab89537669b002da
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-09 17:02:32 +0530

    added stations

--- src/lib/components/ui/select.svelte
+++ src/lib/components/ui/select.svelte
@@ -1,3 +1,9 @@
-<select name="" id="">
-	<option value="Test">Hello</option>
+<script lang="ts">
+	export let values: string[];
+</script>
+
+<select name="" id="" class="px-5 py-4 ring-1 ring-neutral-300 rounded-full">
+	{#each values as value}
+		<option {value}>{value}</option>
+	{/each}
 </select>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,23 @@
 	import Ticket from '$lib/icons/Ticket.svelte';
 	import Cart from '$lib/icons/Cart.svelte';
 	import Select from '$lib/components/ui/select.svelte';
+	import { stationList } from '$lib/stationlist';
+
+	const stations = stationList.map((s) => s.Station_ShortName);
 </script>
 
 <main class="h-screen flex flex-col items-center justify-center">
 	<form action="" method="post" class="flex flex-col gap-16">
 		<h1 class="font-bold text-2xl flex gap-2 text-center">Book Your Ticket <Ticket /></h1>
-		<div>
-			<Select />
+		<div class="flex flex-col gap-5">
+			<div>
+				<p>Origin:</p>
+				<Select values={stations} />
+			</div>
+			<div>
+				<p>Destination:</p>
+				<Select values={stations} />
+			</div>
 		</div>
 		<Button on:click={() => alert(1)}>Make Payment <Cart /></Button>
 	</form>
