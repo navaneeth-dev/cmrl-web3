5052cb4f8e8ccef2ea0ace19e3fa56c97e9c6567
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-12 11:43:27 +0530

    slice account

--- src/lib/components/wallet-connect.svelte
+++ src/lib/components/wallet-connect.svelte
@@ -8,18 +8,8 @@
 		provider = new ethers.BrowserProvider(window.ethereum);
 		signer = await provider.getSigner();
 
-		// const tx = await contract.store(200);
-		// await tx.wait();
-
 		const account = await signer.getAddress();
 		return account.slice(0, 6) + '...' + account.slice(-4);
-	};
-
-	const test = async () => {
-		const contract = new Contract('0xf8e81D47203A594245E36C48e151709F0C19fBe8', abi, signer);
-		const tx2 = await contract.retrieve();
-		const data = await tx2.wait();
-		console.log(data);
 	};
 </script>
 
@@ -34,5 +24,3 @@
 {:catch error}
 	<button class="hover:opacity-75" on:click={connectWallet}>Connect Wallet</button>
 {/await}
-
-<!-- <button on:click={test}>Test</button> -->
