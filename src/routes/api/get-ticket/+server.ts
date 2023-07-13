dc0115bfe3e8e09cfb824577cccad10935f9cd3f
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-13 09:28:21 +0530

    testing pptr

--- src/routes/api/get-ticket/+server.ts
+++ src/routes/api/get-ticket/+server.ts
@@ -1,14 +1,8 @@
 import type { RequestHandler } from './$types';
-import puppeteer from 'puppeteer-core';
-import chromium from '@sparticuz/chromium';
+import puppeteer from 'puppeteer';
 
 export const POST = (async () => {
-	const browser = await puppeteer.launch({
-		args: chromium.args,
-		defaultViewport: chromium.defaultViewport,
-		executablePath: await chromium.executablePath(),
-		headless: chromium.headless
-	});
+	const browser = await puppeteer.launch();
 
 	const page = await browser.newPage();
 
