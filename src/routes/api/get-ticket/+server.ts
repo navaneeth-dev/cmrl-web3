e5631685d384a3fb9c8f298068786497545e9140
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-13 08:49:25 +0530

    test puppeteer

--- src/routes/api/get-ticket/+server.ts
+++ src/routes/api/get-ticket/+server.ts
@@ -1,5 +1,23 @@
 import type { RequestHandler } from './$types';
+import puppeteer from 'puppeteer-core';
+import chromium from '@sparticuz/chromium';
 
-export const POST = (() => {
-	return new Response('hello world');
+export const POST = (async () => {
+	const browser = await puppeteer.launch({
+		args: chromium.args,
+		defaultViewport: chromium.defaultViewport,
+		executablePath: await chromium.executablePath(),
+		headless: chromium.headless
+	});
+
+	const page = await browser.newPage();
+
+	await page.goto('https://example.com');
+
+	const title = await page.title();
+	const content = await page.content();
+
+	await browser.close();
+
+	return new Response(title);
 }) satisfies RequestHandler;
