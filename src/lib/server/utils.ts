6a7267b153e85574c0f5bc287300497736352db5
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-13 10:33:58 +0530

    testing encryption

---
+++ src/lib/server/utils.ts
@@ -0,0 +1,57 @@
+import forge from 'node-forge';
+
+const tokenFromUI = 'UjA%C*F-JaN7x!A%D*G-KamQeThWmZq4';
+
+// Encrypt JSON
+export function getEncrypt(Z: object) {
+	// Generate random IV from strings
+	const R = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz,./;'[]=-)(*&^%$#@!~`";
+	const V = makeRandom(10, R);
+	const tokenFromIV = makeRandom(16, R);
+
+	const G = makeRandom(7, R);
+	// const key = forge.util.createBuffer(tokenFromUI, 'utf8');
+	// const iv = forge.util.createBuffer(tokenFromIV, 'utf8');
+	const key = tokenFromUI;
+	const iv = tokenFromIV;
+
+	console.log(key);
+	console.log(iv);
+
+	const data = forge.util.createBuffer(JSON.stringify(Z));
+	console.log(data.length());
+
+	// Encrypt
+	const cipher = forge.cipher.createCipher('AES-CBC', key);
+	cipher.start({ iv: iv });
+	cipher.update(data);
+	cipher.finish();
+	console.log('finished');
+	const encryptedStr = cipher.output.toHex();
+
+	const z = btoa(V + tokenFromIV + G + encryptedStr);
+	return z;
+}
+
+function makeRandom(Z, R) {
+	let V = '';
+	for (let G = 0; G < Z; G++) V += R.charAt(Math.floor(Math.random() * R.length));
+	return V;
+}
+
+// function decryptResponse(Z) {
+// 	const R = atob(Z);
+// 	G = R.substring(0, 10);
+// 	R.substring(10, 26);
+// 	B = R.substring(26, 33);
+// 	R.substring(33);
+// 	let j = i.enc.Utf8.parse(this.tokenFromUI);
+// 	z = i.enc.Utf8.parse(G);
+
+// 	return i.AES.decrypt(B, j, {
+// 		keySize: 32,
+// 		iv: z,
+// 		mode: i.mode.CBC,
+// 		padding: i.pad.Pkcs7
+// 	}).toString(i.enc.Utf8);
+// }
--- src/routes/api/get-ticket/+server.ts
+++ src/routes/api/get-ticket/+server.ts
@@ -1,17 +1,20 @@
+import { getEncrypt } from '$lib/server/utils';
 import type { RequestHandler } from './$types';
 import puppeteer from 'puppeteer';
 
 export const POST = (async () => {
-	const browser = await puppeteer.launch({ headless: 'new' });
+	const a = getEncrypt({ hello: 'world' });
+	console.log(a);
+	// const browser = await puppeteer.launch({ headless: 'new' });
 
-	const page = await browser.newPage();
+	// const page = await browser.newPage();
 
-	await page.goto('https://example.com');
+	// await page.goto('https://example.com');
 
-	const title = await page.title();
-	const content = await page.content();
+	// const title = await page.title();
+	// const content = await page.content();
 
-	await browser.close();
+	// await browser.close();
 
-	return new Response(title);
+	return new Response('Test');
 }) satisfies RequestHandler;
