4cc3119befe6db88d44e238057fbb3161c3e5de4
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-14 20:45:56 +0530

    changed to dynamic env

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script>
 	import '../app.css';
-	import { PUBLIC_BITCART_URL } from '$env/static/public';
+	import { env } from '$env/dynamic/public';
 </script>
 
 <svelte:head>
-	<script src={PUBLIC_BITCART_URL + '/admin/modal/bitcart.js'}></script>
+	<script src={env.PUBLIC_BITCART_URL + '/admin/modal/bitcart.js'}></script>
 </svelte:head>
 
 <slot />
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,16 @@
 import type { Actions } from './$types';
-import { PUBLIC_BITCART_URL } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
 export const actions = {
 	default: async (event) => {
 		const invoiceCreation = {
 			price: 5,
-			store_id: 'aIrwAMyWYnpHhDCsMbtMeVHadWdKAQwC'
+			store_id: env.PUBLIC_BITCART_STORE_ID
 			// notification_url: 'https://webhook.after.checkout.com/goeshere',
 			// redirect_url: 'https://go.here.after.checkout.com'
 		};
 
-		const response = await fetch(PUBLIC_BITCART_URL + '/api/invoices', {
+		const response = await fetch(env.PUBLIC_BITCART_URL + '/api/invoices', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
