7ca87e735a885f0c4ebba936378c2da502cb2b1a
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-14 17:26:54 +0530

    added tagline to website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,10 @@
 </script>
 
 <main class="h-screen flex flex-col items-center justify-center px-2 max-w-screen-sm mx-auto">
-	<h1 class="font-bold text-2xl text-center mb-16">Book Your Ticket <Ticket /></h1>
+	<div class="mb-16">
+		<h1 class="font-bold text-2xl text-center">Book Your Ticket <Ticket /></h1>
+		<p class="text-neutral-700 mt-1">Buy Chennai metro tickets with crypto</p>
+	</div>
 	<form action="" method="post" class="flex flex-col mb-16">
 		<div class="flex flex-col gap-5 mb-16">
 			<div>
