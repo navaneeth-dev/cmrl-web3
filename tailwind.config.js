968f69101e629a76f84e26fe9905d7e0984725e3
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-15 13:11:21 +0530

    added animate-spin-slow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      colors: {
-        'primary': {
-          300: "#666bb2",
-          DEFAULT: "#4249AE",
-        },
-      },
-      fontFamily: {
-        sans: ['Outfit', 'sans-serif'],
-      }
-    },
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					300: '#666bb2',
+					DEFAULT: '#4249AE'
+				}
+			},
+			fontFamily: {
+				sans: ['Outfit', 'sans-serif']
+			},
+			animation: {
+				'spin-slow': 'spin 3s linear infinite'
+			}
+		}
+	},
+	plugins: []
+};
