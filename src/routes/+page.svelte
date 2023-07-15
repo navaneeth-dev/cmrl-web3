82b9fba88cfd40228dc26c3c97c07a36949b399c
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-15 16:32:39 +0530

    show pay modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,30 @@
 	import Select from '$lib/components/ui/select.svelte';
 	import { stationList } from '$lib/stationlist';
 	import WalletConnect from '$lib/components/wallet-connect.svelte';
-	import type { ActionData } from './$types';
+	import type { ActionData, SubmitFunction } from './$types';
 	import { onMount } from 'svelte';
+	import Spinner from '$lib/icons/Spinner.svelte';
+	import { enhance } from '$app/forms';
+	import { updated } from '$app/stores';
 
 	const stations = stationList.map((s) => s.Station_ShortName);
+	let submitting = false;
+
 	export let form: ActionData;
 
-	onMount(() => {
-		if (form?.id) window.bitcart.showInvoice(form.id);
-	});
+	const pay = (id: string) => window.bitcart.showInvoice(id);
+
+	const handleSubmit: SubmitFunction = () => {
+		submitting = true;
+
+		return async ({ result, update }) => {
+			submitting = false;
+			if (result.type === 'success') {
+				pay(result.data?.id);
+			}
+			await update();
+		};
+	};
 </script>
 
 <main class="h-screen flex flex-col items-center justify-center px-2 max-w-screen-sm mx-auto">
@@ -21,18 +36,25 @@
 		<h1 class="font-bold text-2xl text-center">Book Your Ticket <Ticket /></h1>
 		<p class="text-neutral-700 mt-1">Buy Chennai metro tickets with crypto</p>
 	</div>
-	<form method="post" class="flex flex-col mb-16">
-		<div class="flex flex-col gap-5 mb-16">
-			<div>
-				<p class="mb-6">Origin:</p>
-				<Select values={stations} />
-			</div>
-			<div>
-				<p class="mb-6">Destination:</p>
-				<Select values={stations} />
-			</div>
+	{#if form}
+		<div class="mb-16 text-center flex flex-col items-center">
+			<Spinner />
+			<p class="mt-1 mb-4">Awaiting Payment</p>
 		</div>
-		<Button>Make Payment <Cart /></Button>
-	</form>
+	{:else}
+		<form method="post" class="flex flex-col mb-16" use:enhance={handleSubmit}>
+			<div class="flex flex-col gap-5 mb-16">
+				<div>
+					<p class="mb-6">Origin:</p>
+					<Select values={stations} />
+				</div>
+				<div>
+					<p class="mb-6">Destination:</p>
+					<Select values={stations} />
+				</div>
+			</div>
+			<Button disabled={submitting} loading={submitting}>Make Payment <Cart /></Button>
+		</form>
+	{/if}
 	<WalletConnect />
 </main>
