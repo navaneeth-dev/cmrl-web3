b6bd40bcfec3fd379f797078120961e0144b2a26
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-30 17:00:22 +0530

    remove page.ts shit because we need only client

--- src/routes/ticket/[invoiceId]/+page.svelte
+++ src/routes/ticket/[invoiceId]/+page.svelte
@@ -2,36 +2,53 @@
 	import { goto, invalidate } from '$app/navigation';
 	import Button from '$lib/components/ui/button.svelte';
 	import { onMount } from 'svelte';
-	import type { PageData } from './$types';
+	import { env } from '$env/dynamic/public';
+	import { page } from '$app/stores';
+	const { invoiceId } = $page.params;
 
-	export let data: PageData;
+	const fetchTicket = async () => {
+		const response = await fetch(`${env.PUBLIC_BITCART_URL}/api/invoices/${invoiceId}`);
+		const invoice = await response.json();
+		return invoice;
+	};
+
+	const promise = fetchTicket();
 
 	onMount(() => {
-		const interval = setInterval(() => {
-			invalidate('app:checkTicket');
-		}, 5000);
+		const interval = setInterval(() => {}, 5000);
 
 		return () => clearInterval(interval);
 	});
 </script>
 
-{#if data.invoice.id}
+{#await promise}
+	<main class="h-screen flex flex-col items-center justify-center px-2 max-w-screen-sm mx-auto">
+		<h1 class="text-2xl font-bold">Ticket</h1>
+		<p class="mt-1">Ticket is stored via nft.storage</p>
+		<div class="my-16 flex flex-col items-center">
+			<div class="w-32 h-32 bg-neutral-400 animate-pulse" />
+			<p class="font-mono text-base">Loading...</p>
+		</div>
+		<p class="mb-4">Note: Ticket Validity is 120 mins from the time of entry</p>
+		<Button loading={false} on:click={() => goto('/')}>&leftarrow; Back</Button>
+	</main>
+{:then invoice}
 	<main class="h-screen flex flex-col items-center justify-center px-2 max-w-screen-sm mx-auto">
 		<h1 class="text-2xl font-bold">Ticket</h1>
 		<p class="mt-1">Ticket is stored via nft.storage</p>
 		<div class="my-16 flex flex-col items-center">
 			<img
-				src={`https://${data.invoice.notes}.ipfs.nftstorage.link`}
+				src={`https://${invoice.notes}.ipfs.nftstorage.link`}
 				alt="Ticket QRCode"
 				class="w-32 h-32"
 			/>
-			<p class="font-mono text-base">{data.invoice.id}</p>
+			<p class="font-mono text-base">{invoice.id}</p>
 		</div>
 		<p class="mb-4">Note: Ticket Validity is 120 mins from the time of entry</p>
 		<Button loading={false} on:click={() => goto('/')}>&leftarrow; Back</Button>
 	</main>
-{:else}
+{:catch error}
 	<main class="h-screen flex flex-col items-center justify-center px-2 max-w-screen-sm mx-auto">
 		<h1 class="text-2xl font-bold">Ticket Not Found</h1>
 	</main>
-{/if}
+{/await}
--- src/routes/ticket/[invoiceId]/+page.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '$env/dynamic/public';
-import type { PageLoad } from './$types';
-
-export const load = (async ({ fetch, params, depends }) => {
-	depends('app:checkTicket');
-	const response = await fetch(`${env.PUBLIC_BITCART_URL}/api/invoices/${params.invoiceId}`);
-	const invoice = await response.json();
-	return { invoice };
-}) satisfies PageLoad;
