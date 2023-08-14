6a5359b88eedfa060e876a09d67a51622d29eb8c
Navaneeth Rao <navaneeth@inflorencers.com>
2023-08-14 10:56:13 +0530

    notes to metadata server invoice creation

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,14 +4,14 @@ import { env } from '$env/dynamic/public';
 export const actions = {
 	default: async ({ request }) => {
 		const form = await request.formData();
-		const sourceStationId = form.get('source_station_id');
-		const destStationId = form.get('dest_station_id');
+		const src_id = form.get('source_station_id');
+		const dest_id = form.get('dest_station_id');
 
 		const invoiceCreation = {
 			price: 1,
 			store_id: env.PUBLIC_BITCART_STORE_ID,
 			notification_url: env.PUBLIC_API_URL + '/api/checkTicket',
-			notes: `${sourceStationId}|${destStationId}`
+			metadata: { src_id, dest_id }
 		};
 
 		const response = await fetch(env.PUBLIC_BITCART_URL + '/api/invoices', {
