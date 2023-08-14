530c67a759af3365cd724d91023ab6ed1a835de7
Navaneeth Rao <navaneeth@inflorencers.com>
2023-08-14 09:22:52 +0530

    name prop

--- src/lib/components/ui/select.svelte
+++ src/lib/components/ui/select.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	export let values: string[];
+	export let name: string;
 </script>
 
-<select name="" id="" class="px-5 py-4 ring-1 ring-neutral-300 rounded-full text-ellipsis max-w-xs">
+<select {name} id="" class="px-5 py-4 ring-1 ring-neutral-300 rounded-full text-ellipsis max-w-xs">
 	{#each values as value}
 		<option {value}>{value}</option>
 	{/each}
